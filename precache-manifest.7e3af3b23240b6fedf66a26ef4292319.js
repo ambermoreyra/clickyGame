b4239d276c9c6a037780a39bb201f051f3bd00fd
Kona Marie <ambermoreyra@gmail.com>
2019-09-17 15:39:50 -0600

    Updates

---
+++ precache-manifest.7e3af3b23240b6fedf66a26ef4292319.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88b2ae12031e00588a867bdb30b438a",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "3fb26d6e00778607e9a8",
+    "url": "/clickyGame/static/css/main.fa722d3c.chunk.css"
+  },
+  {
+    "revision": "c5b1ec8be5540a9e641e",
+    "url": "/clickyGame/static/js/2.75122b9b.chunk.js"
+  },
+  {
+    "revision": "3fb26d6e00778607e9a8",
+    "url": "/clickyGame/static/js/main.6ec63f2c.chunk.js"
+  },
+  {
+    "revision": "288ecf9a24215582af99",
+    "url": "/clickyGame/static/js/runtime~main.95528fbf.js"
+  }
+]);
